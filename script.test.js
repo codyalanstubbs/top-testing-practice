b682ffd807b169a18a5f95898d918fe8b171d65d
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-11-21 15:59:21 -0500

    Add tests for calculator

--- script.test.js
+++ script.test.js
@@ -1,5 +1,5 @@
 import {
-    capitalize, reverseString
+    capitalize, reverseString, calculator
 } from './script.js';
 
 test('adds 1 + 2 to equal 3', () => {
@@ -12,4 +12,20 @@ test('capitalize first letter of string only', () => {
 
 test('string reversal', () => {
     expect(reverseString("string")).toBe("gnirts");
+});
+
+test('add 1 and 1', () => {
+    expect(calculator.add(1,1).toBe(2));
+});
+
+test('subtract 1 and 1', () => {
+    expect(calculator.subtract(1,1).toBe(0));
+});
+
+test('divide 1 and 1', () => {
+    expect(calculator.divide(1,1).toBe(1));
+});
+
+test('multiply 1 and 1', () => {
+    expect(calculator.multiply(1,1).toBe(1));
 });
