5e0d88f532187785eb335c2887dc4cf909d30604
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-11-21 16:02:01 -0500

    Remove origin test for script funciton

--- script.test.js
+++ script.test.js
@@ -2,10 +2,6 @@ import {
     capitalize, reverseString, calculator
 } from './script.js';
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(script(1, 2)).toBe(3);
-});
-
 test('capitalize first letter of string only', () => {
     expect(capitalize("plymouth")).toBe("Plymouth");
 });
