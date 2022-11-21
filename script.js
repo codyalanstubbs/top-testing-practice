df6347e001a40fbbd7e838cf40948cd3b8553600
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-11-21 15:44:59 -0500

    Test and create capitalize function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 export function script(a, b) {
     return a + b;
 }
+
+export function capitalize(str) {
+    return str.slice(0,1).toUpperCase() + str.slice(1, str.length).toLowerCase();
+}
--- script.test.js
+++ script.test.js
@@ -1,7 +1,11 @@
 import {
-    script
+    script, capitalize
 } from './script.js';
 
 test('adds 1 + 2 to equal 3', () => {
     expect(script(1, 2)).toBe(3);
+});
+
+test('capitalize first letter of string only', () => {
+    expect(capitalize("plymouth")).toBe("Plymouth");
 });
